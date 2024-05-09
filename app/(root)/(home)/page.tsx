2fba48e3a0960b4b48c34225b6f5ef3380ed8505
Saikiran.Purna <saikiran.purna@engro.io>
2024-05-09 16:32:20 +0530

    dates modified

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,17 +1,69 @@
+'use client';
 import MeetingTypeList from '@/components/MeetingTypeList';
+import { useGetCalls } from '@/hooks/useGetCalls';
+import { useEffect, useState } from 'react';
+import { Call } from '@stream-io/video-react-sdk';
 
 const Home = () => {
   const now = new Date();
+  const [upCommingData, setUpCommingData] = useState('No Meetings Scheduled');
+  const time = now.toLocaleTimeString('en-IN', {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+  const date = new Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(
+    now,
+  );
+  const { upcomingCalls } = useGetCalls();
 
-  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
+  useEffect(() => {
+    if (!upcomingCalls) return;
+    // upcomingCalls[0].Call.state?.startsAt?.toLocaleString()
+    const upCmCalls = upcomingCalls.map((call: Call) => {
+      if ((call as Call).state?.startsAt) {
+        const now = new Date();
+        const startDate = (call as Call).state?.startsAt?.toLocaleString('en-IN', {
+          year: '2-digit',
+          month: '2-digit',
+          day: '2-digit',
+          hour: '2-digit',
+          minute: '2-digit',
+          second:"2-digit"
+        }) ?? '';
+        // const startTime = (call as Call).state?.startsAt?.toLocaleString('en-IN', {
+        //   hour: '2-digit',
+        //   minute: '2-digit',
+        //   second:"2-digit"
+        // }) ?? '';
+        const formattedDate = now.toLocaleDateString('en-IN', {
+          year: '2-digit',
+          month: '2-digit',
+          day: '2-digit',
+          hour: '2-digit',
+          minute: '2-digit',
+          second:"2-digit"
+        });
+        // const formattedTime = now.toLocaleDateString('en-IN', {
+        //   hour: '2-digit',
+        //   minute: '2-digit',
+        //   second:"2-digit"
+        // });
+        if(startDate > formattedDate ){
 
+          return startDate
+        }
+        return '';
+      }
+    }).sort() || []
+    setUpCommingData(upCmCalls[0] || 'No Meetings Scheduled')
+
+  }, [upcomingCalls]);
   return (
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
-            Upcoming Meeting at: 12:30 PM
+            Upcoming Meeting at: {upCommingData}
           </h2>
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
@@ -19,7 +71,6 @@ const Home = () => {
           </div>
         </div>
       </div>
-
       <MeetingTypeList />
     </section>
   );
